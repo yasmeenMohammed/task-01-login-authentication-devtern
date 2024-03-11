772004741222e29ae711182d69ecbb6ee3220453
yasmeenMohammed <145321650+yasmeenMohammed@users.noreply.github.com>
2024-03-11 22:44:09 +0530

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,35 @@
+
+const defaultUsername = "devtern";
+const defaultPassword = "password";
+const defaultUsername2 = "admin";
+const defaultpassword2 = "tiger";
+
+function validateForm() {
+  // Get the values from the input fields
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // Check if the username is empty
+  if (username.trim() === "") {
+    alert("Please enter your username.");
+    return false;
+  }
+
+  // Check if the password is empty
+  if (password.trim() === "") {
+    alert("Please enter your password.");
+    return false;
+  }
+
+  // Check if the entered credentials match the default values
+  if (username === defaultUsername && password === defaultPassword) {
+    // Redirect to successful.html if the credentials are correct
+    window.location.href = "successful.html";
+    return true;
+  } else {
+    // Display an alert if the credentials are incorrect
+    alert("Incorrect username or password.");
+    return false;
+  }
+}
+  
